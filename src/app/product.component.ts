7bd96d344702aeed8c6289ee03e22d2116387673
Abdullah Alsabouni <21061149@stu.omu.edu.tr>
2024-05-29 16:35:29 +0300

    Lessob 5-Pipes

--- src/app/product.component.ts
+++ src/app/product.component.ts
@@ -4,21 +4,38 @@ import { Product } from "./product.model";
 
 @Component({
    selector: "app",
-    template: `<input [(ngModel)] = "email" (keyup.enter) = "onKeyUp()" /> <!-- ngModel kullanarak Two Binding işlemi yapılır -->
-    <br>
-    <span> {{ email }} </span> 
+    template: `
+    <h4> Lower-Upper-Title CasePipe </h4>
+    <p> {{ title1 }}</p>
+    <p> {{ title1 | lowercase }}</p>
+    <p> {{ title1 | uppercase }}</p>
+    <p> {{ title1 | titlecase }}</p> 
+
+    <h4> Date Pipe</h4>
+    <p> {{ today }} </p> <!-- zamanı ms cinsinden yazar-->
+    <p> {{ today | date }} </p>  <!-- May 29, 2024 -->
+
+    <h4> Decimal Pipe </h4>
+    <p> {{ students }} </p>
+    <p> {{ students | number }}</p>
+    <p> {{ price | number }}</p>
+    <p> {{ price | number: '1.2-2' }}</p> <!-- Ondalıklı kısım 2 basamaklı -->
+
+    <h4> currency Pipe</h4>
+    <p> {{ completed | currency: 'EUR'}} </p>
+
+    <h4> Percent Pipe </h4>
+    <p>{{ completed | percent }}</p>
+
     `,
     styleUrls: ["product.component.css"]
 })
 
 
 export class ProductComponent {
-    model: ProductRepository = new ProductRepository();
-    
-
-    email = "email@abdullah.com";
-    
-    onKeyUp(){
-        console.log(this.email); 
-    }
+    today: number = Date.now();
+    title1: string = 'Angular kursu';
+    students: number = 21536;
+    price: number = 395.9923;
+    completed: number = 0.26;
 }
