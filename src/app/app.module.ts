1bb8fc525c52161d31e64f98891c39f8a0b5654c
Abdullah Alsabouni <21061149@stu.omu.edu.tr>
2024-05-30 18:59:15 +0300

    7-Lesson NgIf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { FormsModule } from '@angular/forms';
 import { SummaryPipe } from './summary.pipe';
 
 
+
 @NgModule({
   declarations: [
     ProductComponent,
--- src/app/product.component.ts
+++ src/app/product.component.ts
@@ -1,17 +1,14 @@
 import { Component } from "@angular/core";
 import { ProductRepository } from "./repository.model";
-import { Product } from "./product.model";
+
 
 @Component({
    selector: "app",
-    template: `
-    {{ text | summary: 15}}  <!-- limit = 15 -->
-    `,
-    
+    templateUrl: "product.component.html",
     styleUrls: ["product.component.css"]
 })
 
 
 export class ProductComponent {
-    text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, facere recusandae minus quaerat neque aliquid officia, accusamus error provident at aut sint voluptates ipsa est nemo? Nulla nam temporibus voluptatibus.";
+    model: ProductRepository = new ProductRepository();
 }
